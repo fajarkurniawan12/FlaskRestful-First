df9f60e75cb95b05824190f655cafb35bac67d04
fajarkurniawan12 <32864730+fajarkurniawan12@users.noreply.github.com>
2021-04-13 11:46:38 +0700

    updates

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ class ContohResource(Resource):
         umur = request.form["umur"]
         identitas["nama"] = nama 
         identitas["umur"] = umur 
-        response = {"msg" : "Sukses bosku"}
+        response = {"msg" : "Selamet anda berhasillllllllllll"}
         return response
 
 # setup resourcenya 
